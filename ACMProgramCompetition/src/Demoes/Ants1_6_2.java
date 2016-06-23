e3e58fceba5b7f751bc1648c751df49f62ab9b1b
Git-Yogurt <vm@li.cm>
2016-06-23 10:09:55 +0800

    Finish Demo 1_6_2 Ants Problem

---
+++ ACMProgramCompetition/src/Demoes/Ants1_6_2.java
@@ -0,0 +1,24 @@
+package Demoes;
+
+import java.util.Scanner;
+
+/**
+ * Created by Yogurt on 6/19/16.
+ */
+public class Ants1_6_2 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int L=sc.nextInt();
+        int n=sc.nextInt();
+        int[] lengthToLeft=new int[n];
+        for(int i=0;i<n;i++)
+            lengthToLeft[i]=sc.nextInt();
+        int max=0,min=0;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,Math.max(lengthToLeft[i],L-lengthToLeft[i]));
+            min=Math.max(min,Math.min(lengthToLeft[i],L-lengthToLeft[i]));
+        }
+        System.out.println("max="+max);
+        System.out.println("min="+min);
+    }
+}
