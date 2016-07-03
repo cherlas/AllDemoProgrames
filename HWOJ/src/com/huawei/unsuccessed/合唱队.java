be55eb82da51800398c75cc359da2a6254d330c6
Git-Yogurt <vm@li.cm>
2016-07-03 08:39:31 +0800

    edit 合唱队.java

--- HWOJ/src/com/huawei/unsuccessed/合唱队.java
+++ HWOJ/src/com/huawei/unsuccessed/合唱队.java
@@ -3,37 +3,7 @@ package com.huawei.unsuccessed;
 import java.util.Arrays;  
 import java.util.Scanner;  
   
-class 合唱队 {  
-    private static int[] beautifulScore;  
-    private static int num;  
-    private static String[] datas= null;  
-    public static void main(String[] args) {  
-        @SuppressWarnings("resource")  
-        Scanner sc=new Scanner(System.in);  
-        num=sc.nextInt();  
-        sc.nextLine();  
-        datas=new String[num];  
-        for(int i=0;i<num;i++){  
-            datas[i]=sc.nextLine();  
-        }  
-        int[] times=new int[26];  
-        beautifulScore=new int[num];  
-          
-        for(int i=0;i<num;i++){  
-            for (int k = 0; k < 26; k++) {  
-                times[k]=0;  
-            }  
-            datas[i].toLowerCase();  
-            for(int j=0;j<datas[i].length();j++){  
-                times[datas[i].charAt(j)-97]++;  
-            }  
-            Arrays.sort(times);  
-            for(int j=25;j>=0&&times[j]!=0;j--){  
-                beautifulScore[i]+=(times[j]*(1+j));  
-            }  
-        }  
-        for (int i = 0; i <num; i++) {  
-            System.out.println(beautifulScore[i]);  
-        }  
-    }   
+class 合唱队 {
+    public static void main(String[] args) {
+    }
 } 
