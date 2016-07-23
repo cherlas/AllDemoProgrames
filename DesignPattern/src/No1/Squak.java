3d8fa045ce171d44abaf98420a267c3155338b9b
Git-Yogurt <vm@li.cm>
2016-07-23 09:51:32 +0800

    Squak实现QuackBehavior 接口

---
+++ DesignPattern/src/No1/Squak.java
@@ -0,0 +1,11 @@
+package No1;
+
+/**
+ * Created by Yogurt on 16/7/23.
+ */
+public class Squak implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("Squeak");
+    }
+}
