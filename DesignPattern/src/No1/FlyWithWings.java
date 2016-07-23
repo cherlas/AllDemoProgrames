b187a0c779688136163585a9ac2bb6e2367b703d
Git-Yogurt <vm@li.cm>
2016-07-23 09:44:43 +0800

    FlyWithWings 实现 FlyVBehavior

---
+++ DesignPattern/src/No1/FlyWithWings.java
@@ -0,0 +1,12 @@
+package No1;
+
+/**
+ * Created by Yogurt on 16/7/23.
+ */
+public class FlyWithWings implements FlyVBehavior{
+
+    @Override
+    public void fly() {
+        System.out.println("I'm flying");
+    }
+}
