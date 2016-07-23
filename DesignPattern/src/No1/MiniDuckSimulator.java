4b0d33a389dd03c46a224316c8e493c15129ec27
Git-Yogurt <vm@li.cm>
2016-07-23 10:41:00 +0800

    测试

---
+++ DesignPattern/src/No1/MiniDuckSimulator.java
@@ -0,0 +1,12 @@
+package No1;
+
+/**
+ * Created by Yogurt on 16/7/23.
+ */
+public class MiniDuckSimulator {
+    public static void main(String[] args){
+        Duck mallardDuck=new MallardDuck();
+        mallardDuck.performFly();
+        mallardDuck.performQuack();
+    }
+}
