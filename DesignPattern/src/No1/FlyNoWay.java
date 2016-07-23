7c2e633b61abebf2e58374db66685a05d9ef85cd
Git-Yogurt <vm@li.cm>
2016-07-23 09:46:27 +0800

    FlyNoWay实现FlyVBehavior 接口

---
+++ DesignPattern/src/No1/FlyNoWay.java
@@ -0,0 +1,11 @@
+package No1;
+
+/**
+ * Created by Yogurt on 16/7/23.
+ */
+public class FlyNoWay implements FlyVBehavior {
+    @Override
+    public void fly() {
+        System.out.println("I can't fly");
+    }
+}
