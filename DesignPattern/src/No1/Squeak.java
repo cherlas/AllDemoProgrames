83d68ab2e6f29bffc84f7172b19a4301058560c1
Git-Yogurt <vm@li.cm>
2016-07-23 09:52:21 +0800

    rename

--- DesignPattern/src/No1/Squeak.java
+++ DesignPattern/src/No1/Squeak.java
@@ -3,7 +3,7 @@ package No1;
 /**
  * Created by Yogurt on 16/7/23.
  */
-public class Squak implements QuackBehavior {
+public class Squeak implements QuackBehavior {
     @Override
     public void quack() {
         System.out.println("Squeak");
