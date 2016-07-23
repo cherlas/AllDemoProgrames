23828b3b3bf2a99761cc1973a2d141c1bd1618e8
Git-Yogurt <vm@li.cm>
2016-07-23 09:48:05 +0800

    Quack实现QuackBehavior 接口

---
+++ DesignPattern/src/No1/Quack.java
@@ -0,0 +1,11 @@
+package No1;
+
+/**
+ * Created by Yogurt on 16/7/23.
+ */
+public class Quack implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
